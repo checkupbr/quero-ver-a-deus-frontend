36135394b7a11a99e1d582058a3a9af316796d24
checkupbr <ranieriodonto@yahoo.com.br>
2025-07-28 12:32:31 -0300

    Recreate App.jsx and main.jsx with routing and pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
-function App() {
-  return <h1>Quero Ver a Deus - App funcionando!</h1>
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import React from 'react'; 
+import ReactDOM from 'react-dom/client'; 
+import App from './App'; 
+import './index.css'; 
